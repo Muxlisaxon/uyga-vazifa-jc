8bc425f5527570f99e3bd71a5b62c19c7b676a55
Muxlisaxon <qochqorovamuxlisam@gmail.com>
2023-03-27 16:03:39 +0500

    hygefu

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let Malika = +prompt("summani kiriting")
+let Diyora = +prompt("summani kiriting")
+let Mavzuna = +prompt("summani kiriting")
+let Muxlisa = +prompt("summani kiriting")
+
+function money(Malika, Diyora, Mavzuna, Muxlisa) {
+    if(Malika, Diyora, Mavzuna, Muxlisa >= 150000){
+        return ("mablag yetar");
+    } else{
+        return ("mablag yetarli emas");
+    }
+}
+
+console.log(money(Malika, Diyora, Mavzuna, Muxlisa));
+
+       
+
+
+
+
+
+
